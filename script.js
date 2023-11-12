ce1c86b7d7b3e085fc8cfb1f6d78e86084046c28
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2023-11-12 12:21:02 +0900

    birinchi javascriptda yozgan kodlarim

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
+// number
+console.log(100 * 10);
+console.log(typeof 500);
+
+// string
+console.log("hello");
+console.log(typeof "hello");
+
+// boolean
+console.log(5 <= 5);
+console.log(typeof Boolean(5));
+
+// bigint10
+console.log(999999999999999n + 15n);
+console.log(typeof 999999999999999n + 10n);
+
+// Symbol (false)
+console.log(Symbol("text1") == Symbol("text1"));
 
 
+// object
+let user = { 
+    name: "Oxunjon", 
+    status: "student",
+    age:27 
+};
+console.log(user.name);
+console.log(user.status);
+console.log(typeof user);
 
 
-console.log(typeof 500)
+// array=> container (object)
+let arr = [{name:"samsung"}, {name:"LG"}, 500,"hello", null, undefined];
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(typeof arr);
 
 
+// function
+function biinchiFunction() {
+    console.log("hello g24 students");
+}
 
+biinchiFunction();
+console.log(typeof biinchiFunction);
