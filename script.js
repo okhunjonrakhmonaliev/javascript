6b3eacb13b1c5a148d1c2e0b8b4c77f4d705828f
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2023-11-23 22:52:48 +0900

    for loopda ishladim

--- script.js
+++ script.js
@@ -1,123 +1,126 @@
+// "use strict"
 
-"use strict"
+// let numb = 7;
+// numb % 2 == 0 && console.log("Juft son");
+// numb % 2 != 0 && console.log("Toq son");
 
+// // if
+// if (numb%2==0) {
+//     console.log("Juft son boladi");
+// } else {
+//     console.log("Toq son boladi");
+// }
 
-let numb = 7;
-numb % 2 == 0 && console.log("Juft son");
-numb % 2 != 0 && console.log("Toq son");
+// // ternary (?)
+// numb % 2 == 0 ? console.log("Juft son chiqdi") : console.log("Toq son chiqdi");;
 
-// if
-if (numb%2==0) {
-    console.log("Juft son boladi");
-} else {
-    console.log("Toq son boladi");
+// // =============================================================
+// let ball = 88;
+
+// ball<=100 && ball>=86 && console.log("A");
+// ball<=85 && ball>=61 && console.log("B");
+// ball<=60 && ball>=50 && console.log("C");
+// ball<50 && console.log("Failed");
+
+// // if
+// if (ball<=100 && ball>=86) {
+//   console.log("A degree");
+// } else if (ball<=85 && ball>=61)  {
+//   console.log("B degree");
+// } else if (ball<=60 && ball>=50)  {
+//     console.log("C degree");
+// } else {
+//     console.log("Failed");
+// }
+
+// // ========================================================
+
+// let meva="olma";
+
+// meva==="olma" && console.log("10000");
+// meva==="nok" && console.log("12000");
+// meva==="tarvuz" && console.log("20000");
+// meva!=="olma" && meva!=="nok" && meva!=="tarvuz" && console.log("bunday meva yuq");
+
+// // if
+// if (meva==="olma") {
+//     console.log("10000sum");
+// } else if (meva==="nok") {
+//     console.log("12000sum");
+// } else if (meva==="tarvuz") {
+//     console.log("20000sum");
+// } else {
+//     console.log("bunday meva qolmagan edi");
+// }
+
+// // switch/case
+// switch (meva) {
+//     case "olma" : console.log( "1kg 10000" );break
+//     case "nok" : console.log("1kg 12000");break
+//     case "tarvuz" : console.log("1kg 20000");break
+//     default : console.log("bunday meva yuq edi ertaga olib kelamiz");
+// }
+
+// // ==================================================================
+
+// let bolin=7
+
+// bolin % 3 == 0 && console.log("3ga bolinadi");
+// bolin % 5 == 0 && console.log("5ga bolinadi");
+// // bolinuvchi % 3 == 0 && bolinuvchi % 5 == 0 && console.log("3ga va 5ga bolinadi");
+
+// // if
+// if (bolin % 3 ==0 && bolin % 5 ==0) {
+//     console.log("3ga va 5ga bolinadi va qoldiq qolmaydi");
+// } else if (bolin % 3 ==0) {
+//     console.log("3ga bolinadi va qoldiq qolmaydi");
+// } else if (bolin % 5 ==0) {
+//     console.log("5ga bolinadi va qoldiq qolmaydi");
+// } else { console.log("3ga ham 5ga ham bolinmaydi");}
+
+// // ===========================================================
+
+// let browser="Safari"
+
+// if (browser=="Edge") {
+//     console.log("You've got the Edge!");
+// } else if (browser=="Chrome"){
+
+// } else if (browser=="Firefox"){
+
+// } else if (browser=="Safari"){
+
+// } else if (browser=="Opera"){
+//     console.log("Okay we support these browsers too");
+// }else{
+//     console.log("We hope that this page looks ok!");
+// }
+
+// // ======================================================
+
+// let login="Employee"
+
+// if (login=="Employee") {
+//     console.log("Hello");
+// }else if (login=="Director") {
+//     console.log("Greetings");
+// }else if (login=="") {
+//     console.log("No login");
+// }else {
+//     console.log("Failed");
+// }
+
+for (let i = 1; i <= 10; i++) {
+  i % 2 === 0 ? console.log(i, "Juft son") : console.log(i, "Toq son");
 }
 
+let str = "WebBrainAcadAmya";
 
-// ternary (?)
-numb % 2 == 0 ? console.log("Juft son chiqdi") : console.log("Toq son chiqdi");;
-
-// =============================================================
-let ball = 88;
-
-ball<=100 && ball>=86 && console.log("A");
-ball<=85 && ball>=61 && console.log("B");
-ball<=60 && ball>=50 && console.log("C");
-ball<50 && console.log("Failed");
-
-
-// if
-if (ball<=100 && ball>=86) {
-  console.log("A degree");
-} else if (ball<=85 && ball>=61)  {
-  console.log("B degree");
-} else if (ball<=60 && ball>=50)  {
-    console.log("C degree");
-} else {
-    console.log("Failed");
+let result = "";
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === "a" || (str[i]=== "A")) {
+    result += str[i]
+  }
 }
 
-// ========================================================
-
-let meva="olma";
-
-meva==="olma" && console.log("10000");
-meva==="nok" && console.log("12000");
-meva==="tarvuz" && console.log("20000");
-meva!=="olma" && meva!=="nok" && meva!=="tarvuz" && console.log("bunday meva yuq");
-
-// if
-if (meva==="olma") {
-    console.log("10000sum");
-} else if (meva==="nok") {
-    console.log("12000sum");
-} else if (meva==="tarvuz") {
-    console.log("20000sum");
-} else {
-    console.log("bunday meva qolmagan edi");
-}
-
-// switch/case
-switch (meva) {
-    case "olma" : console.log( "1kg 10000" );break
-    case "nok" : console.log("1kg 12000");break
-    case "tarvuz" : console.log("1kg 20000");break
-    default : console.log("bunday meva yuq edi ertaga olib kelamiz");
-}
-
-
-
-// ==================================================================
-
-
-let bolin=7
-
-bolin % 3 == 0 && console.log("3ga bolinadi");
-bolin % 5 == 0 && console.log("5ga bolinadi");
-// bolinuvchi % 3 == 0 && bolinuvchi % 5 == 0 && console.log("3ga va 5ga bolinadi");
-
-// if
-if (bolin % 3 ==0 && bolin % 5 ==0) {
-    console.log("3ga va 5ga bolinadi va qoldiq qolmaydi");
-} else if (bolin % 3 ==0) {
-    console.log("3ga bolinadi va qoldiq qolmaydi");
-} else if (bolin % 5 ==0) {
-    console.log("5ga bolinadi va qoldiq qolmaydi");
-} else { console.log("3ga ham 5ga ham bolinmaydi");}
-
-// ===========================================================
-
-let browser="Safari"
-
-if (browser=="Edge") {
-    console.log("You've got the Edge!");
-} else if (browser=="Chrome"){
-
-} else if (browser=="Firefox"){
-    
-} else if (browser=="Safari"){
-    
-} else if (browser=="Opera"){
-    console.log("Okay we support these browsers too");
-}else{
-    console.log("We hope that this page looks ok!");
-}
-
-// ======================================================
-
-
-let login="Employee"
-
-if (login=="Employee") {
-    console.log("Hello");
-}else if (login=="Director") {
-    console.log("Greetings");
-}else if (login=="") {
-    console.log("No login");
-}else {
-    console.log("Failed");
-}
-
-
-
-
+console.log("A,a-", result.length);
