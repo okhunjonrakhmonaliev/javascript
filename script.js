748e7c4eb4cdf766dca35aeb27c893d7eb13d033
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2023-11-28 23:27:04 +0900

    for loop,string

--- script.js
+++ script.js
@@ -2,46 +2,10 @@
 
 
 
-let str1 = '#oxunjon#web#brain#'
+for (let i=1; i<=10; i++) {
 
-console.log();
-
-// =======================================
-
-let str2 = "Oxunjon    "
-
-console.log(str2.lengthendsWith('n' || 'N'));
-
-// ==============================================
-
-let str3 = "Web";
-let str4 = "Brain";
-let result1 = str3;
-str3 = str4
-str4 = result1
-
-console.log(str3);
-
-// =============================================
-
-let str5 = "webBrain "
-console.log(str5.repeat(10));
-console.log(str5.replace("web","IT").repeat(10));
-
-// ===================================================
-
-let kabisa = 2023;
-if (kabisa % 4 == 0){
-    console.log("Kabisa yili");
-} else {
-    console.log("Kabisa yili emas");
+    for (j=1; j<=10; j++) {
+       
+        console.log(i,"x",j,"=",i*j);
+    }
 }
-
-// ===================================================
-
-let teks = "#Oxunjon#web#Brain#";
-let result2 = teks.replace("#","(").replace("#",")").replace("#","(").replace("#",")");
-console.log(result2);
-
-
-
