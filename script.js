d58edf2132b3df175753cfbb364ff28dc66fbdaf
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2023-12-16 23:08:30 +0900

    tasks

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Array
+
+// let str = "WebBrain";
+// console.log(str.split(""));
+
+// let arr = [100];
+// let arr1 = 100;
+// console.log(Array.isArray(arr)); true
+// console.log(Array.isArray(arr1)); false
+
+// let arr = ["oxunjon", "Foziljon", "O'tkirbek", "Jaloliddin aka"];
+// console.log(arr[0]); oxunjon;
+// console.log(arr.length); 4
+
+// newArray=================================
+
+// let arr = new Array(100);
+// console.log(arr); []
+// console.log(arr); [ <100 empty items> ]
+// console.log(arr.length); 100
+
+// array updata, add ==================
+
+// let arr = [100, 200,"oxunjon",null, undefined,15,true];2
+// arr[0] = 5;
+// arr[2] = 25;
+// arr[5] = 10;
+// console.log(arr);  [ 5, 200 ]
+// console.log(arr); [ 100, 200, 25 ]
+// console.log(arr); [ 100, 200, 25, <2 empty items>, 10 ]
+// console.log(arr[arr.length - 1]); true
+
 
 
 
@@ -8,12 +40,16 @@
 
 // console.log(arr[0],arr[arr.length - 1]);
 
-
 // ============================================
 
-let arr = [4,10,888,99,100];
+// let arr = [4,10,888,99,100];
 
-let tostring = arr.join();
+// let tostring = arr.join();
 
+// console.log(tostring);
 
-console.log( tostring);
+// let arr = ["olma", "nok", "banan", "olma", "anor"];
+
+// function DeleteMeva(name) {}
+
+// DeleteMeva("olma");
