dc3ea190ae44332df48b1d9b9c7d1fbd95c38ea0
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2023-12-30 14:54:42 +0500

    array methods

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let res = 0;
-const sum = (n) => {
-  if (n === 1) return (res = res + 1);
-  else {
-    res += n;
-    return sum(n - 1);
-  }
-};
+// let res = 0;
+// const sum = (n) => {
+//   if (n === 1) return (res = res + 1);
+//   else {
+//     res += n;
+//     return sum(n - 1);
+//   }
+// };
 
-sum(4);
+// sum(4);
 
-// console.log(res);
+// // console.log(res);
 
+// let arr = ["foziljon","oxunjon","jaloliddin aka","o'tkirbek","shuxrat","madina","sardor"]
 
-let arr = ["foziljon","oxunjon","jaloliddin aka","o'tkirbek","shuxrat","madina","sardor"]
+// // console.log(arr.slice(2,3));
+// console.log(arr.splice(3,4));
 
-// console.log(arr.slice(2,3));
-console.log(arr.splice(3,4));
+let arr = [20, 35, 10, 5, 65, 100];
+
+let filtered = arr.filter((value, index) => value < 50);
+console.log(filtered);
