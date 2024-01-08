681fa9c524f43574f3995589ab53ef7936b58fbe
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-08 16:14:47 +0500

    fetch

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-console.log("started...");
+// console.log("started...");
 
-let server = {
-  id: 1,
-  password: 9999,
-  username: "Test",
-};
+// let server = {
+//   id: 1,
+//   password: 9999,
+//   username: "Test",
+// };
 
-let facebook = (pw, username) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (server.password === pw && server.username === username) {
-        resolve("Welcome to Facebook");
-      } else {
-        reject("Login yoki Pw xato!!!");
-      }
-    }, 2000);
-  });
-};
+// let facebook = (pw, username) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (server.password === pw && server.username === username) {
+//         resolve("Welcome to Facebook");
+//       } else {
+//         reject("Login yoki Pw xato!!!");
+//       }
+//     }, 2000);
+//   });
+// };
 
-async function getData() {
-  try {
-    let user = await facebook(9999, "Test");
-    console.log(user);
-    console.log("Finished...");
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function getData() {
+//   try {
+//     let user = await facebook(9999, "Test");
+//     console.log(user);
+//     console.log("Finished...");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-getData();
+// getData();
+
+
+// fetch API 
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+})
