9565a600c7c29e95274fa502a8fa0312cffd3bdb
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-10 11:35:57 +0500

    DOM

--- DOM.js
+++ DOM.js
@@ -1,9 +1,40 @@
+// DOM => document object model
 
-
-
-
+// General overview
+// console.log(window);
+// console.log(document);
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.title = 'WBA');
 // console.log(document.getElementById("title").innerHTML);
+
+// //////////////////////////////////////////
+
+// console.log(document.getElementById('title'));
+// console.log(document.getElementById('title').innerHTML);
+
+// ///////////////////////////////////////////////
+// js implementtion (domda id,class va tag namelar orali elementlarni tanlab olish)
 
 // const title = document.getElementById("title");
 // const title = document.getElementsByClassName("title");
-const title = document.getElementsByTagName("h1");
+// const title = document.getElementsByTagName("h1");
+
+/////////////////////////////////////////////////
+// general => querySelectors()
+
+// querySelector
+// const general = document.querySelector('#title');
+// const general = document.querySelector('.text');
+// const general = document.querySelector('h1');
+
+/////////////////////////////////////////////////////
+
+// querySelectorAll
+// const general = document.querySelectorAll('#title');
+// const general = document.querySelectorAll('.text');
+// const general = document.querySelectorAll('h1');
+
+/////////////////////////////////////////////////////
+
