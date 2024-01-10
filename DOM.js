90dc04a7ca224a0b29e1c4a5d821a5c9a9defc67
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-10 10:52:14 +0500

    DOM

---
+++ DOM.js
@@ -0,0 +1,9 @@
+
+
+
+
+// console.log(document.getElementById("title").innerHTML);
+
+// const title = document.getElementById("title");
+// const title = document.getElementsByClassName("title");
+const title = document.getElementsByTagName("h1");
