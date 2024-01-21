d8f7b0ae5a14ff1017f81d4f72a0572a333c3e19
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-21 21:59:44 +0500

    promise

--- fetch.js
+++ fetch.js
@@ -15,3 +15,43 @@ async function getData() {
 }
 
 getData();
+
+
+/////////////////////////////////////
+
+
+// console.log('Started...');
+
+// let server = {
+//     id: 1,
+//     password: 9999,
+//     username: 'Test'
+// }
+
+// let facebook = (pw, username) => {
+//     let user = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (server.password === pw && server.username === username) {
+//                 resolve('Welcome to Facebook!!!')
+//             } else {
+//                 reject('Login yoki Pw xato!!!')
+//             }
+//         }, 2000)
+//     })
+    
+//     user
+//     .then((data) => {
+//         console.log(data);
+//         console.log('Finished...');
+//     })
+//     .catch(error => console.log(error))
+// }
+
+// facebook(9999, 'Tes');
+
+// let result = "string" + 10;
+// let result2 = Number ("test")
+// console.log(typeof result2);
+
+let result = String(5*5);
+console.log( result);
--- promise.js
+++ promise.js
@@ -1,28 +1,35 @@
-console.log('Started...');
+// console.log('Started...');
 
-let server = {
-    id: 1,
-    password: 9999,
-    username: 'Test'
-}
+// let server = {
+//     id: 1,
+//     password: 9999,
+//     username: 'Test'
+// }
 
-let facebook = (pw, username) => {
-    let user = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (server.password === pw && server.username === username) {
-                resolve('Welcome to Facebook!!!')
-            } else {
-                reject('Login yoki Pw xato!!!')
-            }
-        }, 2000)
-    })
+// let facebook = (pw, username) => {
+//     let user = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (server.password === pw && server.username === username) {
+//                 resolve('Welcome to Facebook!!!')
+//             } else {
+//                 reject('Login yoki Pw xato!!!')
+//             }
+//         }, 2000)
+//     })
     
-    user
-    .then((data) => {
-        console.log(data);
-        console.log('Finished...');
-    })
-    .catch(error => console.log(error))
-}
+//     user
+//     .then((data) => {
+//         console.log(data);
+//         console.log('Finished...');
+//     })
+//     .catch(error => console.log(error))
+// }
 
-facebook(9999, 'Tes');
+// facebook(9999, 'Tes');
+
+// let result = "string" + 10;
+// let result2 = Number ("test")
+// console.log(typeof result2);
+
+let result = String(5*5);
+console.log( result);
